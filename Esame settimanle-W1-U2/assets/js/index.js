4ba2ea5ebf130f4fc134b85bfe084d50f63ba9d3
Frelser77 <damicosalvatorealessandro@gmail.com>
2023-11-26 17:55:30 +0100

    Aggiornamento

--- Esame settimanle-W1-U2/assets/js/index.js
+++ Esame settimanle-W1-U2/assets/js/index.js
@@ -6,7 +6,7 @@ let button = document.getElementById("btn-start");
 function checkScroll() {
   let scrollPosition = window.scrollY; // Ottiengo la posizione dello scroll
 
-  if (scrollPosition > 300) {
+  if (scrollPosition > 400) {
     // Se la pagina scrolla oltre Xpx, cambia il colore alla navbar
     navbar.style.backgroundColor = "white";
     navbar.style.transition = "1s";
@@ -19,3 +19,17 @@ function checkScroll() {
 }
 
 window.addEventListener("scroll", checkScroll);
+
+const Medium = setInterval(function () {
+  let m = document.querySelectorAll("g");
+  m.forEach((e) => {
+    let random = Math.round(Math.random() * 200);
+    if (random == 2) {
+      let scambio = e.innerHTML;
+      e.innerHTML = "";
+      const refresh = setInterval(function () {
+        e.innerHTML = scambio;
+      }, 1000);
+    }
+  });
+}, 200);
